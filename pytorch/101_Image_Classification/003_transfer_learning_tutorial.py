c5470a93d4c98b34f1dd2a437077bec18c53c118
themis0888 <themis0888@gmail.com>
2018-12-09 16:33:18 +0900

    Label processor & visualization adde

--- pytorch/101_Image_Classification/003_transfer_learning_tutorial.py
+++ pytorch/101_Image_Classification/003_transfer_learning_tutorial.py
@@ -72,30 +72,22 @@ plt.ion()   # interactive mode
 
 # Data augmentation and normalization for training
 # Just normalization for validation
-data_transforms = {
-    'train': transforms.Compose([
+data_transforms = transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
+    ])
 
-data_dir = '/navi/data/input_data/CUB_200_2011/images'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True, num_workers=4)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+
+data_dir = '/navi/data/input_data/CUB_200_2011/images/'
+image_datasets = datasets.ImageFolder(os.path.join(data_dir),
+                                          data_transforms)
+dataloaders = torch.utils.data.DataLoader(image_datasets, batch_size=4,
+                                             shuffle=True)
+
+dataset_sizes = len(image_datasets)
+class_names = image_datasets.classes
 num_class = len(class_names)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -120,7 +112,7 @@ def imshow(inp, title=None):
 
 
 # Get a batch of training data
-inputs, classes = next(iter(dataloaders['train']))
+inputs, classes = next(iter(dataloaders))
 
 # Make a grid from batch
 out = torchvision.utils.make_grid(inputs)
@@ -147,7 +139,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
-
+    counter = 0
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
@@ -164,7 +156,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             running_corrects = 0
 
             # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
+            for inputs, labels in dataloaders:
+                counter += 1
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
@@ -184,11 +177,15 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                         optimizer.step()
 
                 # statistics
-                running_loss += loss.item() * inputs.size(0)
+                cur_loss = loss.item() * inputs.size(0)
+                running_loss += cur_loss
                 running_corrects += torch.sum(preds == labels.data)
+                if counter % 100 == 0:
+                    print('{} Step \tLoss: {:.4f} Acc: {:.4f}'.format(
+                        counter, cur_loss, torch.sum(preds == labels.data)/4))
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            epoch_loss = running_loss / dataset_sizes
+            epoch_acc = running_corrects.double() / dataset_sizes
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
