1563085475dfbe6eca30cc2600999c4306229715
themis0888 <themis0888@gmail.com>
2019-04-02 21:51:44 +0900

    Graph

--- pytorch/101_Image_Classification/004_tensorboardX.py
+++ pytorch/101_Image_Classification/004_tensorboardX.py
@@ -5,14 +5,38 @@ import numpy as np
 import torchvision.models as models
 from torchvision import datasets
 from tensorboardX import SummaryWriter
+import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
 import pdb
+
+
+
+def fig2data ( fig ):
+
+    # draw the renderer
+    fig.canvas.draw ( )
+ 
+    # Get the RGBA buffer from the figure
+    w,h = fig.canvas.get_width_height()
+    buf = np.fromstring ( fig.canvas.tostring_argb(), dtype=np.uint8)
+    buf.shape = ( w, h,4 )
+ 
+    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
+    buf = np.roll ( buf, 3, axis = 2 )
+    return buf / 256
+
 
 resnet18 = models.resnet18(False)
 writer = SummaryWriter()
 sample_rate = 44100
 freqs = [262, 294, 330, 349, 392, 440, 440, 440, 440, 440, 440]
 
-for n_iter in range(50000):
+for n_iter in range(100):
 
     dummy_s1 = torch.rand(1)
     dummy_s2 = torch.rand(1)
@@ -26,19 +50,30 @@ for n_iter in range(50000):
 
     dummy_img = torch.rand(32, 3, 64, 64)  # output from network
     if n_iter % 10 == 0:
+
         x = vutils.make_grid(dummy_img, normalize=True, scale_each=True)
-        print('Hello world! :D')
         # pdb.set_trace()
+        print('Hello world! :D')
+        xx = np.random.normal(size=100)
+        plot = sns.distplot(xx)
+        img = plot.get_figure()
+        img_np = fig2data(img)
+        img_ts = torch.tensor(img_np)
+        img_ts = img_ts.view(4,640,480)
+
+        
+        pdb.set_trace()
 
         writer.add_image('Image', x, n_iter)
-        writer.add_image('Image242', x, n_iter)
+        writer.add_image('Graph', img_ts[:3,:,:], n_iter)
 
         dummy_audio = torch.zeros(sample_rate * 2)
+        """
         for i in range(x.size(0)):
             # amplitude of sound should in [-1, 1]
             dummy_audio[i] = np.cos(freqs[n_iter // 10] * np.pi * float(i) / float(sample_rate))
         writer.add_audio('myAudio', dummy_audio, n_iter, sample_rate=sample_rate)
-
+        """
         writer.add_text('Text', 'text logged at step:' + str(n_iter), n_iter)
 
         for name, param in resnet18.named_parameters():
