aa0e75447150f1d0dd0f46625ab4d5b2a88b787d
themis0888 <themis0888@gmail.com>
2019-04-22 23:33:07 +0900

    seaborn plot

---
+++ file_read_write/seaborn_plot.py
@@ -0,0 +1,34 @@
+import numpy as np 
+import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import seaborn as sns
+from scipy import stats
+import pdb
+
+
+
+classes = np.array(list(range(1,11)))
+
+benchmark = np.random.randn(10)
+portfolio = np.random.randn(10)
+
+df = pd.DataFrame({'Classes': classes,
+                   '#_Wrong': benchmark,
+                   '#_Data': portfolio},
+                    columns = ['Classes','#_Wrong','#_Data'])
+
+                    
+df1 = pd.melt(df, id_vars=['Classes']).sort_values(['variable','value'])
+sns.barplot(x='Classes', y='value', hue='variable', data=df1)
+# plt.xticks(rotation=90)
+plt.ylabel('Error')
+plt.title('Err / Num_data per Class')
+
+plt.savefig('sample.png')
+plt.cla()
+
+
